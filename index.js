9c8a909599e3e57789e2e4899df4dbe151657601
Restem1 <restam.ibnsalih@gmail.com>
2025-03-30 12:29:02 -0700

    finished writing a gma e to playe a single round

--- index.js
+++ index.js
@@ -13,4 +13,27 @@ function getComputerChoice(){
     }
     return computerChoice;
 }
-console.log(getComputerChoice());
+let humanScore = 0;
+let computerScore = 0; 
+function getHumanFunction(){
+    var inputFromUser = prompt().toLowerCase();
+    
+}
+const humanChoice = getHumanFunction();
+const computerChoice = getComputerChoice();
+function playerRound(humanChoice , computerChoice){
+    
+   if(humanChoice === computerChoice){
+        return "Its a tie";
+   }
+   else if((userChoice === "Rock" && computerChoice === "Scissors") || 
+   (userChoice === "Scissors" && computerChoice === "Paper") || 
+   (userChoice === "Paper" && computerChoice === "Rock")){
+        humanScore++;
+        return "you won"
+   }
+   else{
+    computerScore++;
+    return "you lost"
+   }
+}
