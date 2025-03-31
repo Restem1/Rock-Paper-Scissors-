8676b44adaeb62faedb6e176a0777aa3a59a8f16
Restem1 <130650077+Restem1@users.noreply.github.com.>
2025-03-31 07:52:47 -0700

    finishing creating playgame function and debuging the uper function and finishing the project for now

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let humanScore = 0;
+let computerScore = 0; 
 
 function getComputerChoice(){
     var randomChoice = Math.floor((Math.random()*3 )+ 1);
@@ -13,30 +15,51 @@ function getComputerChoice(){
     }
     return computerChoice;
 }
-let humanScore = 0;
-let computerScore = 0; 
+
 function getHumanFunction(){
-    let inputFromUser = prompt().toLowerCase();
+    let inputFromUser = prompt("Choose between [rock , paper , Scissors]").toLowerCase();
     return inputFromUser;   
 }
 
 
 
 function playerRound( humanChoice = getHumanFunction(),computerChoice = getComputerChoice()){
-    console.log(humanChoice);
-    console.log(computerChoice);
+    
    if(humanChoice === computerChoice){
-        return "Its a tie";
+        return `your choice:"${humanChoice}" &  "${computerChoice}":computer choice ,are the same so its a tie`;
    }
    else if(( humanChoice=== "rock" && computerChoice === "scissors") || 
    (humanChoice === "scissors" && computerChoice === "paper") || 
    (humanChoice === "paper" && computerChoice === "rock")){
         humanScore++;
-        return "you won"
+        return `your choice:"${humanChoice}" beats "${computerChoice}":computer choice`
    }
    else{
     computerScore++;
-    return "you lost"
+    return `computer choice: "${computerChoice}" beats "${humanChoice}":your choice`;
    }
+   
+  
   
 }
+
+function playGame(gameRound = prompt("how many round do you want to play")){
+     
+     for (var i = 0 ; i < gameRound; i++){
+         let round= playerRound();
+          console.log(round);
+     }
+     console.log(`Your score = ${humanScore}`);
+   console.log(`computer Score = ${computerScore}`);
+      
+     if (humanScore > computerScore) {
+          return "you won";
+      } else if (computerScore > humanScore) {
+          return "you have lost";
+      } else {
+          return "it's a tie";
+      }
+}
+const result = playGame();
+
+console.log(result);
